505a94f16acea8df49fdad86eef252da161cbee8
zakina <zakina.rostand@gmail.com>
2024-09-28 13:41:26 +0200

    model Epreuve

---
+++ src/main/java/sio/paris2024/model/Epreuve.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sio.paris2024.model;
+
+/**
+ *
+ * @author zakina
+ */
+public class Epreuve {
+    
+    private int id;
+    private String nom ;
+    
+}
