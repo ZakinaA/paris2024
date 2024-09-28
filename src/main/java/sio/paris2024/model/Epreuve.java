02d128f458b29b774d7e33c6f4d63391c67b6255
zakina <zakina.rostand@gmail.com>
2024-09-28 13:43:07 +0200

    model Epreuve : relation MTO vers Sport

--- src/main/java/sio/paris2024/model/Epreuve.java
+++ src/main/java/sio/paris2024/model/Epreuve.java
@@ -12,5 +12,35 @@ public class Epreuve {
     
     private int id;
     private String nom ;
+    private Sport sport ;
+
+    public Epreuve() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Sport getSport() {
+        return sport;
+    }
+
+    public void setSport(Sport sport) {
+        this.sport = sport;
+    }
+    
+    
     
 }
